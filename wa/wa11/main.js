c84ecfce7b8549b69b9e62adf5c20f06ac86d10a
nebisbald <156960004+nebisbald@users.noreply.github.com>
2024-04-04 14:34:53 -0600

    changed to JPG again

--- wa/wa11/main.js
+++ wa/wa11/main.js
@@ -6,7 +6,7 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
-const pics = ['pic1.jpg','pic2.jpg','pic3.jpg','pic4.jpg','pic5.jpg'];
+const pics = ['pic1.JPG','pic2.jpg','pic3.jpg','pic4.jpg','pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
 
