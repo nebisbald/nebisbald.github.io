2ccbd01643e47828e1e5096380bd06aec2576065
nebisbald <156960004+nebisbald@users.noreply.github.com>
2024-04-30 09:24:53 -0600

    added background

--- wa/wa13/wa13.js
+++ wa/wa13/wa13.js
@@ -1,36 +1,27 @@
 //problem 1
 let employees = '{ "employees" : [ {"firstName":"Sam", "department":"tech", "designation":"manager", "salary":"40000", "raiseEligible":"true"} , { "firstName":"Mary", "department":"finance", "designation":"trainee", "salary":"18500", "raiseEligible":"true" }, { "firstName":"Bill", "department":"HR", "designation":"Executive", "salary":"21200", "raiseEligible":"false" } ]}';
-
-
 let employeeObj = JSON.parse(employees);
-
 console.log(employeeObj.employees);
 
 //problem 2
 
 let company = '{"companyName":"Tech Stars", "website":"www.techstars.site", "employees" : [ {"firstName":"Sam", "department":"tech", "designation":"manager", "salary":"40000", "raiseEligible":"true"} , { "firstName":"Mary", "department":"finance", "designation":"trainee", "salary":"18500", "raiseEligible":"true" }, { "firstName":"Bill", "department":"HR", "designation":"Executive", "salary":"21200", "raiseEligible":"true" } ]}';
-
 let companyObj = JSON.parse(company);
-
 console.log(companyObj);
 
 //problem 3
 
 employees = '{ "employees" : [ {"firstName":"Sam", "department":"tech", "designation":"manager", "salary":"40000", "raiseEligible":"true"} , { "firstName":"Mary", "department":"finance", "designation":"trainee", "salary":"18500", "raiseEligible":"true" }, { "firstName":"Bill", "department":"HR", "designation":"executive", "salary":"21200", "raiseEligible":"false" }, { "firstName":"Anna", "department":"tech", "designation":"executive", "salary":"25600", "raiseEligible":"false" } ]}';
-
 employeeObj = JSON.parse(employees);
-
 console.log(employeeObj.employees);
 
 //problem 4
 
 
 let total = 0;
-
 for(let i in employeeObj.employees){
     total += parseInt(employeeObj.employees[i].salary);
 }
-
 console.log(total);
 
 //problem 5
@@ -51,7 +42,6 @@ employeeObj.employees[0].wfh = true;
 employeeObj.employees[1].wfh = false;
 employeeObj.employees[2].wfh = false;
 employeeObj.employees[3].wfh = true;
-
 for(let i in employeeObj.employees){
     console.log(employeeObj.employees[i].firstName);
     console.log("WFH? " + employeeObj.employees[i].wfh);
